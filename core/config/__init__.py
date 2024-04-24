bf9c3396ccff210d92ffad339395efedbbc11a6d
codevoyager1984 <codevoyager.jay@gmail.com>
2024-04-24 16:28:26 +0700

    feat: Enable config embedding models

--- core/config/__init__.py
+++ core/config/__init__.py
@@ -13,11 +13,12 @@ config_data = load_config("config.yaml")
 database_config = config_data.get("database", {})
 vector_config = config_data.get("vector", {})
 sql_store_config = config_data.get("sql_store", {})
-
+embeddings_config = config_data.get("embeddings", {"models": []})
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DOWNLOAD_FOLDER = os.path.join(ROOT_DIR, "download")
 SQLITE_FILE_FOLDER = os.path.join(ROOT_DIR, "sqlite-db")
+MODELS_FOLDER = os.path.join(ROOT_DIR, "models")
 
 if not os.path.exists(DOWNLOAD_FOLDER):
     os.makedirs(DOWNLOAD_FOLDER)
--- core/utils/embedding/__init__.py
+++ core/utils/embedding/__init__.py
@@ -2,8 +2,10 @@ from FlagEmbedding import FlagModel
 import torch
 import os
 from core.utils import ROOT_FOLDER
+from core.config import MODELS_FOLDER, embeddings_config
 
 MODEL_MAP = {}
+
 
 def load_model(model_name):
     global MODEL_MAP
@@ -13,10 +15,11 @@ def load_model(model_name):
     model = FlagModel(
         # 如果本地有下载 model 使用本地的，否则在线下载
         model_path if os.path.exists(model_path) else model_name,
-        use_fp16=True
+        use_fp16=True,
     )  # Setting use_fp16 to True speeds up computation with a slight performance degradation
     MODEL_MAP[model_name] = model
     return model
+
 
 def generate_embedding_of_model(model_name, q):
     model = load_model(model_name)
@@ -27,50 +30,19 @@ def generate_embedding_of_model(model_name, q):
 
 SUPPORTED_EMBEDDING_MODELS = [
     {
-        "name": "BAAI/bge-base-zh-v1.5",
-        "displayName": "BAAI/bge-base-zh-v1.5",
-        "dimension": 768,
-        "enabled": True,
-        "link": "https://huggingface.co/BAAI/bge-base-zh-v1.5",
-        "model_path": os.path.join(ROOT_FOLDER, "models/bge-base-zh-v1.5")
-    },
-    {
-        "name": "jinaai/jina-embeddings-v2-base-en",
-        "displayName": "jinaai/jina-embeddings-v2-base-en",
-        "dimension": 768,
-        "enabled": True,
-        "link": "https://huggingface.co/jinaai/jina-embeddings-v2-base-en",
-        "model_path": os.path.join(ROOT_FOLDER, "models/jina-embeddings-v2-base-en")
-    },
-    {
-        "name": "jinaai/jina-embeddings-v2-small-en",
-        "displayName": "jinaai/jina-embeddings-v2-small-en",
-        "dimension": 512,
-        "enabled": True,
-        "link": "https://huggingface.co/jinaai/jina-embeddings-v2-small-en",
-        "model_path": os.path.join(ROOT_FOLDER, "models/jina-embeddings-v2-small-en")
-    },
-    {
-        "name": "moka-ai/m3e-base",
-        "displayName": "moka-ai/m3e-base",
-        "dimension": 768,
-        "enabled": True,
-        "link": "https://huggingface.co/moka-ai/m3e-base",
-        "model_path": os.path.join(ROOT_FOLDER, "models/m3e-base")
-    },
-    {
-        "name": "text-embedding-ada-002",
-        "displayName": "text-embedding-ada-002 (openai)",
-        "dimension": 1536,
-        "enabled": False,
-        "link": "https://openai.com"
+        "name": item["name"],
+        "displayName": item.get("displayName", item["name"]),
+        "dimension": item["dimension"],
+        "enabled": item.get("enabled", True),
+        "model_path": item.get("modelPath")
+        or os.path.join(MODELS_FOLDER, item["name"]),
     }
+    for item in embeddings_config.get("models", [])
 ]
-
 
 def get_dimension_by_embedding_model(embedding_model):
     for item in SUPPORTED_EMBEDDING_MODELS:
-        if item['name'] == embedding_model:
+        if item["name"] == embedding_model:
             return item["dimension"]
 
     raise Exception(f"不支持的 embedding 模型：{embedding_model}")
@@ -78,7 +50,7 @@ def get_dimension_by_embedding_model(embedding_model):
 
 def get_model_path_by_embedding_model(embedding_model):
     for item in SUPPORTED_EMBEDDING_MODELS:
-        if item['name'] == embedding_model:
+        if item["name"] == embedding_model:
             return item["model_path"]
 
     raise Exception(f"不支持的 embedding 模型：{embedding_model}")
