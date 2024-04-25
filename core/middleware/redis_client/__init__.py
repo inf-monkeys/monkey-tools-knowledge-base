f9333386f43112e6c5ccb92ba42fb94d5f754662
codevoyager1984 <codevoyager.jay@gmail.com>
2024-04-25 10:34:24 +0700

    feat: Support redis cluter

--- core/middleware/redis_client/__init__.py
+++ core/middleware/redis_client/__init__.py
@@ -1,7 +1,20 @@
 import redis
 from core.config import config_data
+from rediscluster import (
+    RedisCluster,
+    ClusterConnectionPool,
+)
 
-redis_config = config_data.get('redis', {})
+redis_config = config_data.get("redis", {})
 
-redis_url = redis_config.get('url')
-redis_client = redis.from_url(redis_url)
+mode = redis_config.get("mode", "standalone")
+
+if mode == "standalone":
+    redis_url = redis_config.get("url")
+    redis_client = redis.from_url(redis_url)
+
+elif mode == "cluster":
+    startup_nodes = redis_config.get("nodes", [])
+    password = redis_config.get("password")
+    pool = ClusterConnectionPool(startup_nodes=startup_nodes, password=password)
+    redis_client = RedisCluster(connection_pool=pool)
