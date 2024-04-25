5d7c533ddd7cf55bd3a992b2a64eca155f86c8e9
codevoyager1984 <codevoyager.jay@gmail.com>
2024-04-25 11:01:17 +0700

    feat: Support redis sentinel mode

--- core/middleware/redis_client/__init__.py
+++ core/middleware/redis_client/__init__.py
@@ -4,6 +4,7 @@ from rediscluster import (
     RedisCluster,
     ClusterConnectionPool,
 )
+from redis.sentinel import Sentinel
 
 redis_config = config_data.get("redis", {})
 
@@ -18,3 +19,12 @@ elif mode == "cluster":
     password = redis_config.get("password")
     pool = ClusterConnectionPool(startup_nodes=startup_nodes, password=password)
     redis_client = RedisCluster(connection_pool=pool)
+
+elif mode == "sentinel":
+    sentinel_nodes = redis_config.get("sentinels", [])
+    sentinel_name = redis_config.get("sentinelName")
+    password = redis_config.get("password")
+    sentinel_nodes = [(item['host'], int(item['port'])) for item in sentinel_nodes]
+    sentinel = Sentinel(sentinel_nodes, password=password)
+    redis_client = sentinel.master_for(sentinel_name)
+
